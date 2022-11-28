df8ef69ecdbd0f2197d7e4af0a6e0e959527a99a
Lexi Dillon <and4bb@virginia.edu>
2022-11-28 13:20:38 -0500

    Created HTML, CSS, and JS files for repository

---
+++ src/app.js
@@ -0,0 +1,101 @@
+let now = new Date();
+let p = document.querySelector("p");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+let month = now.getMonth();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let calendarMonth = months[now.getMonth()];
+
+p.innerHTML = `${day}, ${calendarMonth} ${date}, ${year}, ${hours}:${minutes}`;
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+// answer from Matt
+function displayCurrentWeather(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#weather-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("Virginia Beach");
+
+function getCurrentWeather(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
+function showLocation(position) {
+  let currentLat = position.coords.latitude;
+  let currentLon = position.coords.longitude;
+  let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
+  let unit = "imperial";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLon}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayCurrentWeather);
+}
+
+let currentCity = document.querySelector("#current-city-button");
+currentCity.addEventListener("click", getCurrentWeather);
